b4df516a6210f45b823a392606a4bf5324cc0d95
Goryacheva Antonina <41111696+GoryachevaT@users.noreply.github.com>
2019-07-10 13:26:36 +0300

    Update lab02s.sh

--- my_solutions/lab02/lab02s.sh
+++ my_solutions/lab02/lab02s.sh
@@ -1,8 +1,8 @@
-hadoop jar hadoop-streaming.jar 
-    -D mapred.reduce.tasks=1
-    -input /labs/lab02data/facetz_2015_02_12/ 
-    -output  /user/antonina.goryacheva/res_s 
-    -file mapper_s.py 
-    -mapper "mapper_s.py" 
-    -file reduce_s.py 
-    -reducer "reduce_s.py"
+hadoop jar hadoop-streaming.jar \
+    -D mapred.reduce.tasks=1  \
+    -input /labs/lab02data/facetz_2015_02_12/  \
+    -output  /user/antonina.goryacheva/res_s  \
+    -file mapper_s.py  \
+    -mapper "mapper_s.py"  \
+    -file reduce_s.py  \
+    -reducer "reduce_s.py" \
